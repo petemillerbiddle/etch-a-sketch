72173c67d0dace97bd2186adc45a631de0397200
petemillerbiddle <petemillerbiddle@gmail.com>
2022-05-16 12:07:36 -0400

    Change grid background color on mouse enter event

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const gridContainer = document.querySelector('#grid-container');
 
-let gridDimensions = 4;
+let gridDimensions = 16;
 let gridColumn; 
 let gridSquare;
+
+function listenerTest(e) {
+    //alert(e.target + ' clicked');
+}
 
 for (let i = 0; i < gridDimensions; i++) {
     gridColumn = document.createElement('div');
     for (let j = 0; j < gridDimensions; j++) {
         gridSquare = document.createElement('div');
-        gridSquare.textContent = 'x';
+        gridSquare.textContent = '. .';
         gridColumn.appendChild(gridSquare);
+        gridSquare.addEventListener('mouseenter', function (e) {
+            e.target.style.background = 'blue';
+        });
     }
+    gridColumn.classList.add('column');
     gridContainer.appendChild(gridColumn);
 }
